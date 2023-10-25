4a6db102d457b6a261f6553dcc9a870dee00d007
Olya <naumchik.olya55@gmail.com>
2023-10-25 14:07:39 +0300

    5

--- config/database.js
+++ config/database.js
@@ -3,18 +3,14 @@ dotenv.config()
 
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DIALECT
-})
 
-// module.exports = new Sequelize('verceldb', 'default', 'TRCz0QXj9btD', {
-//     host: 'ep-cold-flower-38311113-pooler.us-east-1.postgres.vercel-storage.com',
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//           require: true,
-//           rejectUnauthorized: false
-//         }
-//       }
-// })
+module.exports = new Sequelize('verceldb', 'default', 'TRCz0QXj9btD', {
+    host: 'ep-cold-flower-38311113-pooler.us-east-1.postgres.vercel-storage.com',
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+})
