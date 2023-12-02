d37a03bf5383436ef30684b5a48bb867e263588d
Olya <naumchik.olya55@gmail.com>
2023-12-02 12:47:39 +0300

    cors3

--- config/cors.config.js
+++ config/cors.config.js
@@ -3,6 +3,7 @@ module.exports = function corsUse(app) {
     app.use(cors({
         origin: '*',
         credentials: true,
+        methods: 'GET,PUT,PATCH,POST,DELETE',
         allowedHeaders: ['Authorization', 'Content-Type'],
         exposedHeaders: ['Authorization', 'Content-Type']
     }))
